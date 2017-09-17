c1f13b762223773ce4ec3a135a8189c3481a4924
Peter May-Ostendorp <peterostendorp@Peters-MacBook-Pro.local>
2017-09-17 09:22:31 -0600

    Cleanup of some web codes and files to remove unnecessary stuff from flaskexample.

--- flaskexample/a_Model.py
+++
@@ -1,8 +0,0 @@
-def ModelIt(fromUser  = 'Default', births = []):
-  in_month = len(births)
-  print('The number born is %i' % in_month)
-  result = in_month
-  if fromUser != 'Default':
-    return result
-  else:
-    return 'check your input'
--- flaskexample/views.py
+++ flaskexample/views.py
@@ -1,5 +1,3 @@
-#import sys
-#sys.path.append('lib/')
 from flask import render_template
 from flask import request
 from flaskexample import app
@@ -15,10 +13,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
-#from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import classification_report, confusion_matrix
-#from sklearn.externals import joblib
 import pickle
 
 #Import our pickled models
@@ -30,45 +25,6 @@ with open('pickles/data_encryption_ADA_policy.pkl', 'rb') as file:
 
 #Which model shall we use?
 model = RFmodel
-
-#Colors for basic text formatting
-#green = '#5cd65c'
-#red = '#e60000'
-
-user = 'peterostendorp' #add your username here (same as previous postgreSQL)                      
-host = 'localhost'
-dbname = 'birth_db'
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-con = None
-con = psycopg2.connect(database = dbname, user = user)
-
-# def index():
-#     return render_template("index.html",
-#        title = 'Home', user = { 'nickname': 'Miguel' },
-#        )
-
-# @app.route('/db')
-# def birth_page():
-#     sql_query = """                                                                       
-#                 SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';          
-#                 """
-#     query_results = pd.read_sql_query(sql_query,con)
-#     births = ""
-#     for i in range(0,10):
-#         births += query_results.iloc[i]['birth_month']
-#         births += "<br>"
-#     return births
-
-# @app.route('/db_fancy')
-# def cesareans_page_fancy():
-#     sql_query = """
-#                SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
-#                 """
-#     query_results=pd.read_sql_query(sql_query,con)
-#     births = []
-#     for i in range(0,query_results.shape[0]):
-#         births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-#     return render_template('caesarian.html',births=births)
 
 @app.route('/')
 @app.route('/index')
