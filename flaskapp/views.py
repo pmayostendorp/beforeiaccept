52895b8028952f7fd64bc632c60a2ea593c25cdc
Peter May-Ostendorp <peterostendorp@Peters-MacBook-Pro.local>
2017-09-19 14:38:33 -0600

    First successful connection of all 7 models to views.py with predict_all_models function.

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -4,7 +4,7 @@ from flaskapp import app
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import psycopg2
-from lib.text_processors import text_process_policy
+from lib.text_processors import text_process_policy, text_paragraph_segmenter
 import pandas as pd
 import numpy as np
 import nltk
@@ -15,15 +15,49 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 import pickle
 
-#Import our pickled models
-with open('pickles/data_encryption_RF_policy.pkl', 'rb') as file:
-    RFmodel = pickle.load(file)
+##Import our pickled models
+# Data encryption
+with open('pickles/data_encryption_NB_segment.pkl', 'rb') as file:
+    data_encryption_model = pickle.load(file)
 
-with open('pickles/data_encryption_ADA_policy.pkl', 'rb') as file:
-    ADAmodel = pickle.load(file)
+#Data retention
+with open('pickles/data_retention_RF_segment.pkl', 'rb') as file:
+    data_retention_model = pickle.load(file)
 
-#Which model shall we use?
-model = RFmodel
+#Do not track
+with open('pickles/do_not_track_NB_segment.pkl', 'rb') as file:
+    do_not_track_model = pickle.load(file)
+
+#First party collection
+with open('pickles/first_party_collection_NB_segment.pkl', 'rb') as file:
+    first_party_collection_model = pickle.load(file)
+
+#Third party sharing
+with open('pickles/third_party_sharing_RF_segment.pkl', 'rb') as file:
+    third_party_sharing_model = pickle.load(file)
+
+#User access/deletion
+with open('pickles/user_access_NB_segment.pkl', 'rb') as file:
+    user_access_model = pickle.load(file)
+
+#Policy changes
+with open('pickles/policy_change_NB_segment.pkl', 'rb') as file:
+    policy_change_model = pickle.load(file)
+
+    
+#A helper function to run a bunch of models stored in a dict
+def predict_all_models(model_dict,segment_list):
+    """
+    This takes a dict of BeforeIAccept style models as input and provides
+    a dict output with result keys corresponding to the input model keys,
+    evaluated on a list of input policy segments.
+    """
+    names = model_dict.keys()
+    result = {}
+    for name in names:
+        result[name] = model_dict[name].predict(segment_list)
+    
+    return result
 
 @app.route('/')
 @app.route('/index')
@@ -33,14 +67,35 @@ def text_input():
 
 @app.route('/output')
 def text_output():
-  #pull policy text from the input field and store it
-  policy_text = request.args.get('policy_text')
-    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-  #print(policy_text)
-  policy_text_processed = text_process_policy(policy_text)
-  #print(policy_text_processed)
-  result = model.predict([policy_text_processed])
-  #print(result)
-  result_text = 'DOES' if result == 1 else "DOESN'T"
-  
-  return render_template("output.html", policy_text = policy_text, result_text = result_text)
+    #pull policy text from the input field and store it
+    policy_text = request.args.get('policy_text')
+    segment_list = text_paragraph_segmenter(policy_text)
+    segments_processed = [text_process_policy(segment) for segment in segment_list]
+    
+    #DEBUGGING PURPOSES
+    for segment in segments_processed:
+        print(segment)
+        
+    #Evaluate all of the models
+    model_dict = {
+        'data_encryption':data_encryption_model,
+        'data_retention':data_retention_model,
+        'do_not_track':do_not_track_model,
+        'first_party_collection':first_party_collection_model,
+        'third_party_sharing':third_party_sharing_model,
+        'user_access':user_access_model,
+        'policy_change':policy_change_model}
+    
+    #Store results for each model category as dataframe, then sum up occurrences found.
+    results = pd.DataFrame(predict_all_models(model_dict,segments_processed))
+    #DEBUGGING PURPOSES
+    for col in list(results.columns):
+        print(results[col])
+    results = results.sum()
+    
+    #TO DO
+    #Post-process results to achieve good document-level classification.
+    result = results['data_encryption']
+    result_text = 'DOES' if result >= 1 else "DOESN'T"
+
+    return render_template("output.html", policy_text = policy_text, result_text = result_text)
