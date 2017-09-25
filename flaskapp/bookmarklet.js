39eb9c53e8a46fe3326ee92666c7404a884c2b1a
Peter May-Ostendorp <peterostendorp@Peters-MacBook-Pro.local>
2017-09-25 15:11:21 -0600

    Update bookmarklet to open in new window.

--- flaskapp/bookmarklet.js
+++ flaskapp/bookmarklet.js
@@ -1,1 +1,5 @@
+/* OPEN IN SAME WINDOW, DISABLED
 javascript:location.href='http://www.beforeiaccept.info/output?url_text='+encodeURIComponent(location.href)+'&policy_text='
+*/
+/* OPEN IN NEW WINDOW, CURRENTLY IMPLEMENTED */
+javascript:window.open('http://www.beforeiaccept.info/output?url_text='+encodeURIComponent(location.href)+'&policy_text=')
